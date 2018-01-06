c9406302ecb5b13825d5f09abf065bf7311567da
laras126 <laras126@gmail.com>
2018-01-06 11:33:08 -0800

    Comment cleanup

--- inc/helpers.php
+++ inc/helpers.php
@@ -4,12 +4,12 @@
    * Helper function to prepare a sources module
    * @param string slug of the category term
 	 * @var int $POSTS_NUM Constant for number of posts to return
-   * @return array An array data used in the template
+   * @return array An array data to be used in the template
    */
 
 function kmg_get_sources_module_data($slug) {
 
-	$POSTS_NUM = 3; // Always return 3 sources
+	$POSTS_NUM = 3;
 
 	$cat = get_category_by_slug( $slug );
 	$count = $cat->count;
@@ -23,11 +23,11 @@ function kmg_get_sources_module_data($slug) {
 
 	$posts = new Timber\PostQuery($query_args);
 
-	$return_array = array(
+	$template_data = array(
 		'posts' => $posts,
 		'more_count' => $more_count
 	);
 
-	return $return_array;
+	return $template_data;
 
 }
