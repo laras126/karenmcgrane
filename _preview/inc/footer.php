2add74a8159b8469562f392eae3e101bbf948276
beep <me@ethanmarcotte.com>
2018-02-20 15:30:51 -0500

    footer updates

--- _preview/inc/footer.php
+++ _preview/inc/footer.php
@@ -1,12 +1,17 @@
 </main>
 
 <footer id="footer" class="region site-footer">
-    <p>Copyright © Karen McGrane.<?php if ( strpos ( $bodyClass, "sect-home" ) === false ) { ?> <a href="tmpl-home.php">Go home</a>.<?php } ?></p>
+    <p>
+        <a href="#www-karenmcgrane-com">Back to top</a>.
+    <?php if ( strpos ( $bodyClass, "sect-home" ) === false ) { ?>
+        <a href="tmpl-home.php">Go home</a>.
+    <?php } ?>
+    </p>
 
     <div class="legalese">
         <h2 class="hed-legalese">End user license agreement</h2>
 
-        <p>By interacting with this website you acknowledge you are aware of the existence of Karen McGrane and agree that she brings delight to humankind. Your use of this website constitutes an unconditional agreement that you will use it only for a) learning more about Karen McGrane b) initiating further communication with Karen McGrane c) pleasant reminiscing about past encounters with Karen McGrane. The information contained in this website is subject to change without notice. Void where prohibited by law.</p>
+        <p><strong>Copyright © Karen McGrane.</strong> By interacting with this website you acknowledge you are aware of the existence of Karen McGrane and agree that she brings delight to humankind. Your use of this website constitutes an unconditional agreement that you will use it only for a) learning more about Karen McGrane b) initiating further communication with Karen McGrane c) pleasant reminiscing about past encounters with Karen McGrane. The information contained in this website is subject to change without notice. Void where prohibited by law.</p>
     </div>
 </footer>
 
