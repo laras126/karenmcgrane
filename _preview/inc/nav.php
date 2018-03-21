817ea1c9760dd94beb74c45d779005f3a4512ca4
beep <me@ethanmarcotte.com>
2018-03-21 14:21:48 -0400

    updated nav icons

--- _preview/inc/nav.php
+++ _preview/inc/nav.php
@@ -1,32 +1,35 @@
+<?php
+$rootpath = $_SERVER['DOCUMENT_ROOT'];
+?>
         <nav>
             <ul class="nav-list">
                 <li class="nav-item"<?php if ( isset( $bodyClass ) && strpos ( $bodyClass, "sect-articles" ) !== false ) { ?> aria-describedby="current-page"<?php } ?>>
                     <a href="#">
-                        <img src="../dist/assets/svg/articles.svg" alt="" />
+                        <?php include( $rootpath . "/wp-content/themes/karenmcgrane/inc/svg/articles.svg" ); ?>
                         Articles
                     </a>
                 </li>
                 <li class="nav-item"<?php if ( isset( $bodyClass ) && strpos ( $bodyClass, "sect-talks" ) !== false ) { ?> aria-describedby="current-page"<?php } ?>>
                     <a href="#">
-                        <img src="../dist/assets/svg/talks.svg" alt="" />
+                        <?php include( $rootpath . "/wp-content/themes/karenmcgrane/inc/svg/talks.svg" ); ?>
                         Talks
                     </a>
                 </li>
                 <li class="nav-item"<?php if ( isset( $bodyClass ) && strpos ( $bodyClass, "sect-books" ) !== false ) { ?> aria-describedby="current-page"<?php } ?>>
                     <a href="#">
-                        <img src="../dist/assets/svg/books.svg" alt="" />
+                        <?php include( $rootpath . "/wp-content/themes/karenmcgrane/inc/svg/books.svg" ); ?>
                         Books
                     </a>
                 </li>
                 <li class="nav-item"<?php if ( isset( $bodyClass ) && strpos ( $bodyClass, "sect-sources" ) !== false ) { ?> aria-describedby="current-page"<?php } ?>>
                     <a href="#">
-                        <img src="../dist/assets/svg/sources.svg" alt="" />
+                        <?php include( $rootpath . "/wp-content/themes/karenmcgrane/inc/svg/sources.svg" ); ?>
                         Sources
                     </a>
                 </li>
                 <li class="nav-item"<?php if ( isset( $bodyClass ) && strpos ( $bodyClass, "sect-contact" ) !== false ) { ?> aria-describedby="current-page"<?php } ?>>
                     <a href="#">
-                        <img src="../dist/assets/svg/contact.svg" alt="" />
+                        <?php include( $rootpath . "/wp-content/themes/karenmcgrane/inc/svg/contact.svg" ); ?>
                         Contact
                     </a>
                 </li>
