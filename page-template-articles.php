931e7e8a43866835e923cc7a7e2b074270266b6d
laras126 <laras126@gmail.com>
2018-03-31 17:18:04 -0700

    Add source to articles landing

--- page-template-articles.php
+++ page-template-articles.php
@@ -3,20 +3,28 @@
  * Template name: Articles
  */
 
+$post = new TimberPost();
 $context = Timber::get_context();
+$source_cat = get_field('sources_on_articles', 'option');
 
 $posts_args = array(
 	'post_type' => 'post',
-	'posts_per_page' => 10
+	'posts_per_page' => 10,
+	'orderby' => 'date',
+	'order' => 'ASC'
 );
 
 $sources_args = array(
 	'post_type' => 'source',
-	'category__in' => get_field('sources_on_articles', 'option'),
-	'posts_per_page' => 10
+	'category__in' => $source_cat,
+	'posts_per_page' => 10,
+	'orderby' => 'date',
+	'order' => 'ASC'
 );
 
+$context['post'] = $post;
 $context['posts'] = new Timber\PostQuery($posts_args);
+$context['source_cat'] = new Timber\Term($source_cat);
 $context['sources'] = new Timber\PostQuery($sources_args);
 
 Timber::render( 'page-articles.twig', $context );
