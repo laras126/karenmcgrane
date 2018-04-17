c919ebebe7696b82f899dfb3b26b68908fb82b91
laras126 <laras126@gmail.com>
2018-04-17 09:39:13 -0700

    Disable talks archive

--- inc/custom-types.php
+++ inc/custom-types.php
@@ -93,7 +93,7 @@
 		'show_in_admin_bar'   => true,
 		'show_in_nav_menus'   => true,
 		'can_export'          => true,
-		'has_archive'         => true,
+		'has_archive'         => false,
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'rewrite'             => $rewrite,
