e45ba6751e3dbd66a4ae701fad7d6d9a01efb1c5
laras126 <laras126@gmail.com>
2018-04-21 11:02:53 -0700

    Add comments and About Karen count

--- page-articles.php
+++ page-articles.php
@@ -1,6 +1,11 @@
 <?php
+
 /**
  * Template name: Articles
+ *
+ * Articles displays one column of actual articles "By Karen". These are posts.
+ * The other column, "About Karen" displays sources catgegorized "About Karen".
+ * This category is selected in an ACF option.
  */
 
 $POSTS_PER_PAGE = 10;
@@ -9,6 +14,7 @@ $post = new TimberPost();
 $context = Timber::get_context();
 $source_cat = get_field('sources_on_articles', 'option');
 $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+
 $posts_args = array(
 	'post_type' => 'post',
 	'posts_per_page' => $POSTS_PER_PAGE,
@@ -25,14 +31,22 @@ $sources_args = array(
 	'order' => 'ASC'
 );
 
+// Add main queries to context.
 $context['post'] = $post;
 $context['posts'] = new Timber\PostQuery($posts_args);
 $context['source_cat'] = new Timber\Term($source_cat);
 $context['sources'] = new Timber\PostQuery($sources_args);
 
+/**
+ * Prepare "Browse X More" counts.
+ * These link to paginatied lists of the query.
+ */
 $posts_count = wp_count_posts('post')->publish;
+$sources_count = get_term($source_cat, 'category')->count;
 $context['by_karen_count'] = $posts_count - $POSTS_PER_PAGE;
+$context['about_karen_count'] = $sources_count - $POSTS_PER_PAGE;
 
+// Get data for additional sources module.
 $data = kmg_sources_archive($module_count, $excluded_cats);
 $context['remaining_cat_ids'] = $data['remaining_cat_ids'];
 
