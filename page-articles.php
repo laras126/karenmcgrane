acad0d366a1c296fbfa1ca755498c580dbf2baf8
laras126 <laras126@gmail.com>
2018-04-21 10:36:58 -0700

    Add articles post count

--- page-articles.php
+++ page-articles.php
@@ -3,13 +3,15 @@
  * Template name: Articles
  */
 
+$POSTS_PER_PAGE = 10;
+
 $post = new TimberPost();
 $context = Timber::get_context();
 $source_cat = get_field('sources_on_articles', 'option');
 $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 $posts_args = array(
 	'post_type' => 'post',
-	'posts_per_page' => 10,
+	'posts_per_page' => $POSTS_PER_PAGE,
 	'orderby' => 'date',
 	'order' => 'ASC',
 	'paged' => $paged
@@ -18,7 +20,7 @@ $posts_args = array(
 $sources_args = array(
 	'post_type' => 'source',
 	'category__in' => $source_cat,
-	'posts_per_page' => 10,
+	'posts_per_page' => $POSTS_PER_PAGE,
 	'orderby' => 'date',
 	'order' => 'ASC'
 );
@@ -27,5 +29,11 @@ $context['post'] = $post;
 $context['posts'] = new Timber\PostQuery($posts_args);
 $context['source_cat'] = new Timber\Term($source_cat);
 $context['sources'] = new Timber\PostQuery($sources_args);
+
+$posts_count = wp_count_posts('post')->publish;
+$context['by_karen_count'] = $posts_count - $POSTS_PER_PAGE;
+
+$data = kmg_sources_archive($module_count, $excluded_cats);
+$context['remaining_cat_ids'] = $data['remaining_cat_ids'];
 
 Timber::render( 'page-articles.twig', $context );
