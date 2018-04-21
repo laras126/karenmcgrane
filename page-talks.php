b8382202e848502f7f795e58e1f805cbce78ca1a
laras126 <laras126@gmail.com>
2018-04-21 09:23:32 -0700

    Move additional sources to partial, add to talks

--- page-talks.php
+++ page-talks.php
@@ -8,9 +8,11 @@ $talk_args = array(
 	'posts_per_page' => -1
 );
 
+$data = kmg_sources_archive($module_count, $excluded_cats);
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 $context['talks'] = Timber::get_posts($talk_args);
+$context['remaining_cat_ids'] = $data['remaining_cat_ids'];
 
 Timber::render( array( 'page-talks.twig', 'page.twig' ), $context );
